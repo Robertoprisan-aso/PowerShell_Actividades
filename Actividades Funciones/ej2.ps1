8cfe203eaca31e798ba948915311cc24854956e2
Robert Oprisan <orobert291@gmail.com>
2026-01-23 13:19:00 +0100

    EJERCICIOS ACABADOS POWERSHELL - FUNCIONES

---
+++ Actividades Funciones/ej2.ps1
@@ -0,0 +1,8 @@
+﻿$usuarios = Import-Csv "C:\Users\Administrador\Desktop\actividades\PowerShell_Actividades\Actividades Funciones"
+
+foreach ($usuario in $usuarios) { 
+    Write-Host "Nombre: $(usuario.Nombre)"
+    Write-Host "Apellidos: $(usuario.Apellidos)"
+    Write-Host "Grupo: $(usuario.Grupo)"
+    Write-Host "---- TERMINADO -----"
+}
