a0783556f2a86aa074091ac96053322d3212f0d4
Robert Oprisan <orobert291@gmail.com>
2026-02-06 13:48:21 +0100

    script p4

---
+++ script_powershell_p4/script_perfiles_moviles.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module ActiveDirectory
+
+$ServerUNC = "\\192.168.50.10"
+$ShareName = "Empresa_users$"
+$GroupName = "Personal"
+
+$users = Get-ADGroupMember -Identity $GroupName -Recursive | Where-Object { $_.objectClass -eq "user" }
+
+foreach ($u in $users) {
+    $sam = $u.SamAccountName
+    $profilePath = "$ServerUNC\$ShareName\$sam"
+    Set-ADUser -Identity $sam -ProfilePath $profilePath
+}
